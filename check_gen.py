f60841496cfaca057478c72238d7385f3244d91b
linkdom <donglinkang2021@163.com>
2025-09-30 08:09:57 +0000

    [feat]check the pretrained model, print the total parameters count in the script

--- check_gen.py
+++ check_gen.py
@@ -8,8 +8,11 @@ from cs336_basics.generate import generate
 from cs336_basics.config import TrainConfig
 
 # Load model from checkpoint
+# the best model of openwebtext
 ckpt_path = "/inspire/hdd/global_user/donglinkang-253108120084/standford-cs336/assignment1-basics/outputs/multiruns/2025-09-28_20-56-56/4/ckpt_19999.pt"
+# the best model of tinystories
 # ckpt_path = "/inspire/hdd/global_user/donglinkang-253108120084/standford-cs336/assignment1-basics/outputs/multiruns/2025-09-28_20-36-53/4/ckpt_19999.pt"
+ckpt_path = "/inspire/hdd/global_user/donglinkang-253108120084/standford-cs336/assignment1-basics/outputs/runs/2025-09-30_06-59-52/ckpt_39999.pt"
 # get config file
 cfg_path = Path(ckpt_path).parent / ".hydra" / "config.yaml"
 cfg:TrainConfig = OmegaConf.load(cfg_path)
@@ -20,6 +23,8 @@ model = TransformerLM(**cfg.model).to(device)
 checkpoint = torch.load(ckpt_path, map_location=device)
 model.load_state_dict(checkpoint['model_state_dict'])
 print(f"Loaded model from {ckpt_path}")
+total_params = sum(p.numel() for p in model.parameters())
+print(f"Total number of parameters: {total_params/1e6:.2f}M")
 
 model.eval()
 tokenizer = Tokenizer.from_file(cfg.data.tokenizer_path)
@@ -32,7 +37,6 @@ generated_output = tokenizer.decode(
         max_new_tokens=1000, 
         block_size=cfg.model.context_length,
         temperature=0.6,
-        top_p=0.95
     )[0].tolist()
 )
 print("\n--- Generated Text ---")
