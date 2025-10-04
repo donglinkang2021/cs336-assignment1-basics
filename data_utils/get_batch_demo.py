3b1cf57957e7e0426265005a8c57a3f7d5d12f21
linkdom <donglinkang2021@163.com>
2025-10-04 12:30:29 +0000

    [fix]begin to train vocab_size=32000 on openwebtext to see the true val/loss

--- data_utils/get_batch_demo.py
+++ data_utils/get_batch_demo.py
@@ -3,13 +3,13 @@ import numpy as np
 from pathlib import Path
 from cs336_basics.data import get_batch
 
-tokenizer_path = "hf_tokenizer/tinystories/tokenizer.json"
+tokenizer_path = "hf_tokenizer/openwebtext-32k/tokenizer.json"
 tokenizer = Tokenizer.from_file(tokenizer_path)
 
-data_path = Path("data/tinystories")
+data_path = Path("data/openwebtext-32k")
 train_data = np.memmap(data_path / 'train.bin', dtype=np.uint16, mode='r')
 
 x, y = get_batch(train_data, 4, 256, "cuda")
 print(x.shape, y.shape)
-
+print(x[0].tolist())
 print(tokenizer.decode(x[0].tolist()))
---
+++ scripts/train_owt32k_best_wo_muon.sh
@@ -0,0 +1,49 @@
+uv run train.py \
+    model.vocab_size=32000 \
+    data.path=data/openwebtext-32k \
+    data.tokenizer_path=hf_tokenizer/openwebtext-32k/tokenizer.json \
+    training.batch_size=512 \
+    training.max_iters=2500 \
+    training.is_compile=true \
+    optimizer.warmup_iters=250 \
+    'logger.run_name=owt-vs32k-w_compile-wo_muon-bs${training.batch_size}-ms${training.max_iters}-ws${optimizer.warmup_iters}'
+
+uv run train.py \
+    model.vocab_size=32000 \
+    data.path=data/openwebtext-32k \
+    data.tokenizer_path=hf_tokenizer/openwebtext-32k/tokenizer.json \
+    training.batch_size=256 \
+    training.max_iters=5000 \
+    training.is_compile=true \
+    optimizer.warmup_iters=500 \
+    'logger.run_name=owt-vs32k-w_compile-wo_muon-bs${training.batch_size}-ms${training.max_iters}-ws${optimizer.warmup_iters}'
+
+uv run train.py \
+    model.vocab_size=32000 \
+    data.path=data/openwebtext-32k \
+    data.tokenizer_path=hf_tokenizer/openwebtext-32k/tokenizer.json \
+    training.batch_size=128 \
+    training.max_iters=10000 \
+    training.is_compile=true \
+    optimizer.warmup_iters=1000 \
+    'logger.run_name=owt-vs32k-w_compile-wo_muon-bs${training.batch_size}-ms${training.max_iters}-ws${optimizer.warmup_iters}'
+
+uv run train.py \
+    model.vocab_size=32000 \
+    data.path=data/openwebtext-32k \
+    data.tokenizer_path=hf_tokenizer/openwebtext-32k/tokenizer.json \
+    training.batch_size=64 \
+    training.max_iters=20000 \
+    training.is_compile=true \
+    optimizer.warmup_iters=2000 \
+    'logger.run_name=owt-vs32k-w_compile-wo_muon-bs${training.batch_size}-ms${training.max_iters}-ws${optimizer.warmup_iters}'
+
+uv run train.py \
+    model.vocab_size=32000 \
+    data.path=data/openwebtext-32k \
+    data.tokenizer_path=hf_tokenizer/openwebtext-32k/tokenizer.json \
+    training.batch_size=32 \
+    training.max_iters=40000 \
+    training.is_compile=true \
+    optimizer.warmup_iters=4000 \
+    'logger.run_name=owt-vs32k-w_compile-wo_muon-bs${training.batch_size}-ms${training.max_iters}-ws${optimizer.warmup_iters}'
